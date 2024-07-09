858c426329e336b543aa94d5306721d59612ae64
ntusfa <ntusfa@gipo.ntu.edu.tw>
2024-07-09 13:46:32 +0800

    connected redis

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
 from flask import Flask
+import redis
+
+# connect to redis
+r = redis.Redis(
+  host='helpful-mako-37953.upstash.io',
+  port=6379,
+  password='AZRBAAIncDEzNDZlNjU0YTViNDE0YmE4YmQ4YWJkZjE1MjU3ZmU2NnAxMzc5NTM',
+  ssl=True
+)
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return r.get('name')
 
 if __name__ == '__main__':
     app.run()
